52b7d7676612ceb42943ffd6bc3eb97ed7a32ad5
Matthew Brandyberry <mbrandy@us.ibm.com>
2014-03-04 13:06:02 -0600

    PPC support for node source.

--- src/node.cc
+++ src/node.cc
@@ -438,7 +438,9 @@ static inline const char *errno_string(int errorno) {
 #endif
 
 #ifdef ENOTEMPTY
+# if ENOTEMPTY != EEXIST
   ERRNO_CASE(ENOTEMPTY);
+# endif
 #endif
 
 #ifdef ENOTSOCK
@@ -661,7 +663,9 @@ const char *signo_string(int signo) {
 #endif
 
 #ifdef SIGLOST
+# if SIGLOST != SIGABRT
   SIGNO_CASE(SIGLOST);
+# endif
 #endif
 
 #ifdef SIGPWR
